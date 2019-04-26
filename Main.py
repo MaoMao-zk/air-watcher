3095fb6d648c68e4f9b6ce8492c36f06e0493091
MaoMao-go <maomaogo.zhang@gmail.com>
2019-04-26 17:54:36 +0800

    添加温湿度检测

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import SendData
 import time
 from DartWZS import wzs, DartMode
+from sense_hat import SenseHat
+
+sense = SenseHat()
 
 wzs.initialize()
 
@@ -13,13 +16,23 @@ wzs.ReadOnce()
 while 1:
     try:
         print time.localtime(time.time())
+
+        # Get HCHO
         wzs.SendReadCMD()
         hcho = wzs.ReadOnce()
         if hcho < 0:
             print "fail to get HCHO"
         print 'Get HCHO = %f'%hcho
-        SendData.send_hcho(hcho)
-        print 'Sent.'
+
+        # Get Temperature & Humidity
+        temperature = sense.get_temperature()
+        humidity = sense.get_humidity()
+        pressure = sense.get_pressure()
+        print 'Get temperature = {}, humidity = {}, pressure = {}'.format(temperature, humidity, pressure)
+
+        # Send data to server
+        SendData.send_air_data(HCHO = hcho, Temperature = temperature, Humidity = humidity)
+        print 'Data sent.'
     except Exception as ex:
         print ex
     finally:
--- SendData.py
+++ SendData.py
@@ -12,3 +12,26 @@ def send_hcho(hcho):
         return
 
     print response.read()
+
+def send_air_data(HCHO = None, Temperature=None, Humidity=None, TVOC=None, CO2=None):
+    values={}
+    values['api_key'] = WRITE_KEY
+    if HCHO is not None:
+        values['field1'] = HCHO
+    if Temperature is not None:
+        values['field2'] = Temperature
+    if Humidity is not None:
+        values['field3'] = Humidity
+    if TVOC is not None:
+        values['field4'] = TVOC
+    if CO2 is not None:
+        values['field5'] = CO2
+
+    data = urllib.urlencode(values)
+    try:
+        response = urllib2.urlopen(url=BASE_URL, data=data, timeout=10)
+    except Exception as ex:
+        print ex
+        return
+
+    print response.read()
