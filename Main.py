0521269360de0f66852e00a66e0f5c634909d7a9
MaoMao-go <maomaogo.zhang@gmail.com>
2019-06-21 15:19:16 +0800

    Support show data manually.

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import SendData
 import time
 from DartWZS import wzs, DartMode
-from sense_hat import SenseHat
+from sense_hat import SenseHat, ACTION_RELEASED
 from SGP30 import my_sgp30
 
 my_sgp30.initialize()
@@ -16,6 +16,27 @@ wzs.SwitchMode(DartMode.Manual)
 wzs.ReadOnce()
 wzs.SendReadCMD()
 wzs.ReadOnce()
+
+# Show HCHO when press UP
+def pushed_up(event):
+    if event.action != ACTION_RELEASED:
+        wzs.SendReadCMD()
+        hcho = wzs.ReadOnce()
+        if hcho < 0:
+            print ("fail to get HCHO")
+        print ('Get HCHO = %f'%hcho)
+        sense.show_message('HCHO = %.3f mg/m^3'%hcho)
+
+# Show TVOC when press UP
+def pushed_down(event):
+    if event.action != ACTION_RELEASED:
+        tvoc = my_sgp30.read_TVOC()
+        print ('Get TVOC = %dppb'%tvoc)
+        sense.show_message('TVOC = %d ppb'%tvoc)
+
+
+sense.stick.direction_up = pushed_up
+sense.stick.direction_down = pushed_down
 
 while 1:
     try:
